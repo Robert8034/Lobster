32758f123df51fc63d8861e16c5f82ae0c32fb26
Robert Dams <400338@student.fontys.nl>
2020-02-18 13:39:54 +0100

    Add project files.

---
+++ Lobster.Client.Services.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lobster.Client.Services.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 1;
+            Assert.IsTrue(x == 1);
+        }
+    }
+}
---
+++ Lobster.Server.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lobster.Server.Services
+{
+    public class Class1
+    {
+    }
+}
